d63cc78bbcf8f5cd413fa1918417add8ee873e60
Ryan Westlake <ryancwestlake@gmail.com>
2017-01-24 14:50:50 -0700

    [ADD]: get and post methods to folders endpoint

--- src/server.js
+++ src/server.js
@@ -1,16 +1,52 @@
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
+const bodyParser = require('body-parser');
+const md5 = require('md5');
 
-app.get('/', function (req, res) {
+app.set('port', process.env.PORT || 3001);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.locals.folders = {
+  fone: {
+    url1: {
+      actual: 'www.espn.com'
+    }
+  },
+  ftwo: {
+    url2: {
+      actual: 'www.facebook.com'
+    }
+  }
+}
+
+app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.get('/test', function(req, res) {
-  res.send('on the test route')
+app.get('/folders', (req, res) => {
+  res.json(app.locals.folders)
 });
 
-app.set('port', process.env.PORT || 3001);
-app.listen(app.get('port'), function () {
+app.post('/folders', (req, res) => {
+  const { name } = req.body
+  const id = md5(name)
+  app.locals.folders[id] = name
+  res.json({ id, name })
+})
+
+app.get('/folders/:name', (req, res) => {
+  const { name } = req.params
+  const folder = app.locals.folders[name]
+
+  res.json(folder)
+})
+
+
+
+app.listen(app.get('port'), () => {
   console.log('Example app listening on port 3001!');
 });
