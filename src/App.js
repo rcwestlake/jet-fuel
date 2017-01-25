639255fb10f8d3f7c158e2103e63ddc3d5a37360
Alex Pilewski <pilewski.alex@gmail.com>
2017-01-25 12:46:23 -0700

    [ADD] Input Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Input from './components/Input'
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      input: ''
+    }
+  }
+
+  handleChange(location){
+    let userInput = location.target.value;
+    this.setState({input: userInput});
+  }
+
   render() {
     return (
       <div className="App">
-        
+        <section>
+          <Input handleChange={(event)=>this.handleChange(event)}/>
+        </section>
       </div>
     );
   }
---
+++ src/components/Input.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Input extends Component {
+  render() {
+    return(
+      <span>
+        <input
+        placeholder="Enter your guess!"
+        onChange={(event) => this.props.handleChange(event)}
+        />
+      </span>
+    )
+  }
+};
+
+module.exports = Input;
